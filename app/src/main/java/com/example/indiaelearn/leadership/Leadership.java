ad7f48bdc60356bf1496873726a135fedf184ce6
Saurabh Gupta <Saurabh Gupta>
2017-02-23 00:12:33 +0530

    Added the YouTube api for video playback. Made PayUmoney start working

--- app/src/main/java/com/example/indiaelearn/leadership/Leadership.java
+++ app/src/main/java/com/example/indiaelearn/leadership/Leadership.java
@@ -1,13 +1,67 @@
 package com.example.indiaelearn.leadership;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Leadership extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+public class Leadership extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+
+    private Button bt;
+    private YouTubePlayerView youTubeView;
+    private static final int RECOVERY_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leadership);
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize("AIzaSyBqE39PS0DD3qmWqQhRrXmh1j_lWNI0T68 ", this);
+        bt = (Button) findViewById(R.id.button01);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+        if (!wasRestored) {
+            youTubePlayer.cueVideo("kOmOnQ1DvUs"); // Plays https://www.youtube.com/watch?v=fhWaJi1Hsfo
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), youTubeInitializationResult.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize("AIzaSyBqE39PS0DD3qmWqQhRrXmh1j_lWNI0T68 ", this);
+        }
+    }
+
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return youTubeView;
+    }
+
+    public void goToSo (View view) {
+        goToUrl ( "https://www.payumoney.com/paybypayumoney/#/5410243B8CA6EA3107FB017B5D3CD911");
+    }
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
 
